217cb9c5d17c88342bbe16c02d0f2a7b0fb6adc7
PCloud <pcloud63514@gmail.com>
2022-04-10 17:58:53 +0900

    Update

--- applications/web-next-ohlot/components/NavBar.tsx
+++ applications/web-next-ohlot/components/NavBar.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import {useRouter} from "next/router";
-import styles from './NavBar.module.css'
 
 // React Router Dom과 비슷한 Link로, Anchor를 꼭 넣을 필요는 없다. inspect 해보면 Anchor가 자동으로 생성되기 때문. 단 Anchor에 대해 상세히 설정할 내용이 있다면 직접 Anchor를 선언해야한다.
 export default function NavBar() {
@@ -8,14 +7,25 @@ export default function NavBar() {
     return (
         <nav>
             <Link href={"/"}>
-                <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
+                <a className={router.pathname === "/" ? "active" : ""}>Home</a>
             </Link>
             <Link href={"/about"}>
                 About
             </Link>
             <Link href={"/counter"}>
-                <a className={`${styles.link} ${router.pathname === "/counter" ? styles.active : ""}`}>Counter</a>
+                <a className={router.pathname === "/counter" ? "active" : ""}>Counter</a>
             </Link>
+            <style jsx>{`
+                nav {
+                    background-color: tomato;
+                }
+                a {
+                    text-decoration: none;
+                }
+                .active {
+                    color: blue;
+                }
+            `}</style>
         </nav>
     )
 }

5a42e5cf63c018e4951763da97e3ab8d44f5bf33
PCloud <pcloud63514@gmail.com>
2022-04-11 20:53:13 +0900

    Update

--- applications/web-next-ohlot/pages/index.tsx
+++ applications/web-next-ohlot/pages/index.tsx
@@ -1,18 +1,16 @@
 import Title from "../components/Title";
 import WordBox from "../components/WordBox";
 import DndLayout from "../components/DndLayout";
+import CollisionObject from "../components/CollisionObject";
 
 export default function Home() {
     return (
         <div>
             <Title title={"Home"}/>
-            <DndLayout>
-                <WordBox message={"안녕!"}/>
-                <WordBox message={"반가워요!"}/>
-                <WordBox message={"좋은 날이에요!"}/>
-                <WordBox message={"기분은 어때요?"}/>
-                <WordBox message={"좋은 생각만 해요"}/>
-            </DndLayout>
+            <CollisionObject/>
+            <CollisionObject/>
+            <CollisionObject/>
+            <CollisionObject/>
         </div>
     )
 }

a84e5323ffb1dcc8b9b3aed4795e545de29afb5a
PCloud <pcloud63514@gmail.com>
2022-04-25 23:31:38 +0900

    끝이 없네! 끝이 없어!

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-# ./gradlew 파일 실행할 줄 알아야함
-# bootClean bootJar bootRun 할줄 알아야함
-# 특정 서버를 대상으로할지 전체를 대상으로 할지 지정할 수 있어야함
-# 지정 방식은 all 또는 배열 형태로 각 서비스 명을 입력하는 것
-
-# 할 수 있는 일. dockerImage 배포, jar 배포, jar 실행
-# applications 폴더 읽을 수 있어야함
+MSG_1="서비스 실행"
+MSG_2="실행 서비스 목록 조회"
+MSG_3="실행 서비스 종료"
+MSG_4="서비스 파일 build"
+MSG_5="Docker Image 배포"
+MSG_6="build & Docker Image 배포"
 
 function applications_show() {
   echo "==== 서비스 목록 ===="
@@ -14,11 +13,12 @@ function applications_show() {
   for service in ${SERVICES[@]}; do
     echo ${service}
   done
-  echo "=================="
+  echo "====================="
 }
 
 function read_applications() {
-  read -a READ_SERVICES -p "입력 > "
+  echo "실행하고자 하는 서비스를 입력해주세요. (배열입력 또는 입력하지 않을 경우 전체 서비스 실행입니다.)"
+  read -a READ_SERVICES -p "입력(ENTER) > "
 
   if [ -z "$READ_SERVICES" ]; then
     READ_SERVICES+=(${SERVICES[@]})
@@ -29,35 +29,49 @@ function read_applications() {
   for service in ${READ_SERVICES[@]} ; do
     echo ${service}
   done
-
-  echo "======================"
+  echo "=========================="
 }
 
-function jar_run() {
+function build_run() {
   clear
-  echo "Jar 실행 서비스"
-  echo "실행 가능한 서비스 목록 불러오는 중"
-  echo "실행하고자 하는 서비스를 입력해주세요. (배열입력 또는 입력하지 않을 경우 전체 서비스 실행입니다.)"
+  echo "[${MSG_1}]"
   applications_show
   read_applications
+
+  for appName in ${READ_SERVICES[@]}; do
+    echo ${appName} "build start"
+    echo `nohup ./gradlew applications:${appName}:bootRun &`
+  done
+    echo "good!!!"
+
 }
 
-function jar_deployment() {
-  echo "[2]. Jar 배포"
+function build_applications() {
+  echo "[${MSG_4}]"
+  applications_show
+  read_applications
+
+  for service in ${READ_SERVICES[@]}; do
+    if [ ${service} == 'app-*' ]; then
+      echo ${service}
+    fi
+  done
 }
 
 function docker_image_deployment() {
-  echo "[3]. Docker Image 배포"
+  echo "[${MSG_5}]"
 }
 
-function jar_and_docker_image_deployment() {
-  echo "[4]. Jar & Docker Image 배포"
+function build_and_docker_image_deployment() {
+  echo "[${MSG_6}]"
 }
 
-echo "[1]. Jar 실행"
-echo "[2]. Jar 배포"
-echo "[3]. Docker Image 배포"
-echo "[4]. Jar & Docker Image 배포"
+echo "[1]. ${MSG_1}"
+echo "[2]. ${MSG_2}"
+echo "[3]. ${MSG_3}"
+echo "[4]. ${MSG_4}"
+echo "[5]. ${MSG_5}"
+echo "[6]. ${MSG_6}"
 
 # shellcheck disable=SC2162
 read CHOICE
@@ -65,16 +79,16 @@ clear
 
 case ${CHOICE} in
 1)
-  jar_run
-  ;;
-2)
-  jar_deployment
-  ;;
-3)
-  docker_image_deployment
+  build_run
   ;;
 4)
-  jar_and_docker_image_deployment
+  build_applications
+  ;;
+5)
+  docker_image_deployment
+  ;;
+6)
+  build_and_docker_image_deployment
   ;;
 *)
   echo "Error"
